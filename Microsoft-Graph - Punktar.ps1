edde456c28f64a301048dff5eff7a9fdf14ee616
Hafsteinn Thordarson <haffimt@gmail.com>
2020-10-25 22:34:07 +0000

    added a example of new-mguser

--- Microsoft-Graph - Punktar.ps1
+++ Microsoft-Graph - Punktar.ps1
@@ -1,1 +1,3 @@
 Convertfrom-Json '{"forceChangePasswordNextSignIn":"true","forceChangePasswordNextSignInWithMfa":"false","password": "string"}' -AsHashtable
+
+New-MgUser -UserPrincipalName tester2@mulathing.is -DisplayName "Hafsteinn Tester2" -PasswordProfile $a -MailNickname "tester2" -AccountEnabled
